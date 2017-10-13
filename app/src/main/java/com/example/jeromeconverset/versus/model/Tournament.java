aa1503cb675f601b658506fb4858949b58fc7dc7
david keichinger <d.keichinger@ideotechnologies.com>
2017-10-13 22:24:29 +0200

    [Refactor] - Never ever use French nor accent for Class Name

---
+++ app/src/main/java/com/example/jeromeconverset/versus/model/Tournament.java
@@ -0,0 +1,8 @@
+package com.example.jeromeconverset.versus.model;
+
+/**
+ * Created by jeromeconverset on 12/10/2017.
+ */
+
+public class Tournament {
+}
---
+++ app/src/main/java/com/example/jeromeconverset/versus/model/TournamentDay.java
@@ -0,0 +1,9 @@
+package com.example.jeromeconverset.versus.model;
+
+/**
+ * Created by jeromeconverset on 12/10/2017.
+ */
+
+public class TournamentDay {
+
+}
